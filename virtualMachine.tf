3c41fbd0bead3229e1d676adfa9676e710fc7abb
Anushka Visapure <anushka.visapure@betaque.com>
2023-01-30 14:09:05 +0530

    modify 4 deploy.yml file

--- virtualMachine.tf
+++ virtualMachine.tf
@@ -21,6 +21,6 @@ resource "azurerm_linux_virtual_machine" "tf-vm1" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCttlKKjcoeD1le9IhcRdJdZQjI0zBhs2aTyCoM4o39H34eAbtVWgrqJYRVtUqluWlOL14SQ/apFcB/lvU2SR0kK4gXQxatK2Rc3cBGpHX5RajRwTIHEnHSTj9/PdJI8xVcQwHFmwahssZ6cmyvNPoZY/dSEVX69+fqP2+oNQfoUMo9/RIORtQFSjRiFnwpVVBqLtofTuIh2SQUcPbirwkIYkXQ11zrK7tAzL26OSma7DoXlwSfAkcLsjynG08nzQlXnsG9MNbPZPfN3VFG3ytIOSN6P9UI60F95+7SbxSjCyDB34M21UMBPCiYORUGABzs8O1j45VzixcNB3oZfp4wsrLN/ksQI9gGS6kR7UsBzx9xmA7/RqIukthsH/eU69MN/a4gRO8RXEa7SPXz2YytBrac1K+VhzMOMXy/SOlG4K470Lloo7I9wBs7u8nXgZ/CzToF0oXsp8ELxNCV8jouHW3Et8imXRgfP9fFvWGrVg8Zi6/3e4KSbh8XIUBbv1c= ubuntu@ubuntu-IdeaPad-3-15ITL6"
   }
 }
