a4a2f7d7d8a78779a6430f5533367b9d813bf637
Anushka Visapure <anushka.visapure@betaque.com>
2023-02-06 19:09:33 +0530

    commit 15

--- providers.tf
+++ providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "=2.23.0"
+    }
+  }
+}
 provider "azurerm" {
+  use_oidc = true
   features {}
 }
