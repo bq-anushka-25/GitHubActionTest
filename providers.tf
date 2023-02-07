6e741d43369b029eee8e8f1091f67376cb01b7c3
Anushka Visapure <anushka.visapure@betaque.com>
2023-02-07 13:36:33 +0530

    test

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.7.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = ">= 3.7.0"
+#     }
+#   }
+# }
 provider "azurerm" {
   features {}
   use_oidc = true
